c6967721c21a4fbb62bf131df15cc816e3df4793
GameScriptor <computorgenius101@gmail.com>
2020-12-19 15:14:42 -0800

    Ares can now write to an output file

--- src/main.go
+++ src/main.go
@@ -10,28 +10,67 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 0 {
-		fmt.Println("missing the file argument")
+	if len(os.Args) != 3 {
+		fmt.Println("Missing the input and output arguments. The command should look like this: ares <input> <output>")
 		return
 	}
 
-	file, err := ioutil.ReadFile(os.Args[1])
-
+	var inputFile, err = ioutil.ReadFile(os.Args[1])
 	if err != nil {
-		fmt.Println("there was an error reading that file")
+		fmt.Println("There was an error reading that file")
 		fmt.Println(err)
 		return
 	}
 
-	scanner := lexer.New(string(file))
+	compiled := compileAres(string(inputFile))
+	fmt.Println("Successfully compiled!")
 
-	p := parser.New(scanner)
+	createOutFile()
+	writeOut(compiled)
+}
+
+func createOutFile() {
+	_, err := os.Stat(os.Args[2])
+	if os.IsNotExist(err) {
+		var file, err = os.Create(os.Args[2])
+
+		fmt.Println("That output file was not found, creating it...")
+
+		if err != nil {
+			fmt.Println("There was an error in creating that file")
+			fmt.Println(err)
+			return
+		}
+
+		defer file.Close()
+	}
+}
+
+func writeOut(output string) {
+	file, err := os.OpenFile(os.Args[2], os.O_RDWR, 0644)
+	fmt.Println("Writing output...")
+
+	if err != nil {
+		fmt.Println("There was an error opening the output file")
+		fmt.Println(err)
+		return
+	}
+
+	if _, err = file.Write([]byte(output)); err != nil {
+		fmt.Println("There was an error writing to that file")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+}
+
+func compileAres(input string) string {
+	p := parser.New(lexer.New(input))
 	tree := p.ParseProgram()
-
-	//fmt.Println(tree)
 
 	generator := &gen.Gen{Program: tree}
 	generator.Generate()
 
-	fmt.Println(generator.Output)
+	return generator.Output
 }
