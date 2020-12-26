732f6fc2ff40028a36eeef8ab55107f049f2edd4
GameScriptor <computorgenius101@gmail.com>
2020-12-26 11:35:28 -0800

    added extra code documentation

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -2,15 +2,18 @@ package ast
 
 import "heron/src/token"
 
+//Program contains all the abstract syntax tree nodes
 type Program struct {
 	Rules []Selector
 }
 
+//Rule represents the key-value pair of a CSS property
 type Rule struct {
 	Name  string
 	Value string
 }
 
+//Selector represents a CSS rule
 type Selector struct {
 	SelectorText string
 
--- src/token/token.go
+++ src/token/token.go
@@ -1,28 +1,37 @@
 package token
 
+//TokenType represents the type of token
 type TokenType string
 
+//Token represents a token
 type Token struct {
 	Type    TokenType
 	Literal string
 	Loc     Loc
 }
 
+//Loc represents a location in the program
 type Loc struct {
 	Row int
 	Col int
 }
 
 const (
+	//IDENT is short for identifier. It names the languages entities, this could be a property value or name.
 	IDENT = "identifier"
 
-	//Delimiters
-	EOS       = "EOS"
-	SEMICOLON = ";"
-	COLON     = ":"
+	//EOS is an end-of-statement. It is typically represented as a semicolon
+	EOS = "EOS"
 
+	//COLON is a delimiter is used for separating rule properties
+	COLON = ":"
+
+	//LBRACE is a delimiter that signifies the start of a rule
 	LBRACE = "{"
+
+	//RBRACE is a delimiter that signifies the end of a rule
 	RBRACE = "}"
 
+	//EOF signifies the end of a file
 	EOF = "EOF"
 )
